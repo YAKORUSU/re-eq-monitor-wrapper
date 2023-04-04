3e0c9a530129d2b1cbc6084c84b31588d20e0199
YAKORUSU <95697718+YAKORUSU@users.noreply.github.com>
2023-04-04 13:03:15 +0900

    Update submain.py
    API昨日の復帰＆更新

--- submain.py
+++ submain.py
@@ -1,7 +1,15 @@
 import requests
 import datetime
 import time
+from fastapi import FastAPI, Body
+from starlette.websockets import WebSocket
+import syslog
 
+app = FastAPI()
+#取得したJsonを格納
+body = {}
+# websocketで接続中のクライアントを識別するためのIDを格納
+clients = {}
 
 while True:
     # 現在時刻を取得
@@ -15,10 +23,45 @@ while True:
     response = requests.get(url)
 
     if response.status_code == 200:
-        json_data = response.json()
-        print(json_data)
+        body = response.json()
+        if body["type"] == "eew" and body["report"] == "1" and int(body["intensity"]) >= 3:
+            for client in clients.values():
+                await client.send_text("{}".format(body))
+            syslog.syslog(syslog.LOG_INFO, 'SentAlert:{}'.format(body))
+
+        # 緊急地震速報以外はキャンセル扱いだが送信してクライアント側で処理
+        elif body["type"] == "pga_alert_cancel":
+            for client in clients.values():
+                await client.send_text("{}".format(body))
+            syslog.syslog(syslog.LOG_DEBUG, 'SentCancelAlert:{}'.format(body))
+        else:
+            syslog.syslog(syslog.LOG_DEBUG, 'NotSentAlert:{}'.format(body))
     else:
         print("HTTP GET Request Failed: ", response.status_code)
 
     # 1秒待つ
     time.sleep(1)
+
+@app.get("/")
+def read():
+    return {"Result": "ok"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept()
+
+    # クライアントを識別するためのIDを取得
+    key = ws.headers.get('sec-websocket-key')
+    clients[key] = ws
+
+    # TODO debuglog
+    syslog.syslog(syslog.LOG_DEBUG, 'ConnectedClientList:{}'.format(clients))
+
+    try:
+        while True:
+            data = await ws.receive_text()
+    except Exception as e:
+        syslog.syslog(syslog.LOG_DEBUG, '{}:{}'.format(type(e),e))
+        # TODO close状態で下記のコネクションクローズするとさらにRuntimeError出るのでコメントアウト中
+        #await ws.close()
+        del clients[key]
